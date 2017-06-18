cc5639c1d19aa6c1ba796ce0fec444998a270bf4
Wander <lywander@gmail.com>
2017-06-17 22:33:37 -0700

    Add plane generator and ambient light

---
+++ Assets/HoloKitSDK/Internal/Scripts/ARKitWrapper/ARAmbient.cs
@@ -0,0 +1,31 @@
+using System.Runtime.InteropServices;
+using UnityEngine.XR.iOS;
+using UnityEngine;
+
+namespace HoloKit
+{
+    public class ARAmbient : MonoBehaviour
+    {
+
+        private Light l;
+		private UnityARSessionNativeInterface m_Session;
+
+        public void Start()
+        {
+#if !UNITY_EDITOR
+            l = GetComponent<Light>();
+			m_Session = UnityARSessionNativeInterface.GetARSessionNativeInterface ();
+#endif
+        }
+#if !UNITY_EDITOR
+        public void Update()
+        {
+            // Convert ARKit intensity to Unity intensity
+            // ARKit ambient intensity ranges 0-2000
+            // Unity ambient intensity ranges 0-8 (for over-bright lights)
+            float newai = m_Session.GetARAmbientIntensity();
+            l.intensity = newai / 1000.0f;
+        }
+#endif
+    }
+}
