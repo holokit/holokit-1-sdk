1af7a2792c7439f31b571c0a0f4041e776d89e93
Wander <lywander@gmail.com>
2017-06-20 22:02:02 -0700

    holokit by default

--- Assets/HoloKitSDK/Internal/Scripts/Input/HoloKitInputManager.cs
+++ Assets/HoloKitSDK/Internal/Scripts/Input/HoloKitInputManager.cs
@@ -117,6 +117,13 @@ namespace HoloKit
         None = 0,
         Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
         A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+
+        UtopiaA = F,
+        UtopiaB = N,
+        UtopiaC = T,
+        UtopiaD = N,
+        UtopiaFire1 = V,
+        UtopiaFire2 = G,
     }
 
     public enum HoloKitInputType
