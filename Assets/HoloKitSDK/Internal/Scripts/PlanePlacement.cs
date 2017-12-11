a90686d4cc2d339ccf6e6ab153638d5077f697af
pestantium@gmail.com <pestantium@gmail.com>
2017-12-11 17:59:44 +0200

    + Quad mesh with scale 1 + Whale mesh for placing + Debug cube mesh + Change plane detect texture + Placement script improvement: enents, place position by touch/center, rotate to camera, press on ui fix + Removed HoloKitGazeTarget from scene/prefab (future gaze improvement)

--- Assets/HoloKitSDK/Internal/Scripts/PlanePlacement.cs
+++ Assets/HoloKitSDK/Internal/Scripts/PlanePlacement.cs
@@ -9,32 +9,75 @@ namespace HoloKit
 {
     public class PlanePlacement : MonoBehaviour
     {
+        public enum PlacePointer : int
+        {
+            CenterScreen = 0,
+            Touch = 1
+        }
+
         public UnityEvent onPlace = new UnityEvent();
         public UnityEvent onHide = new UnityEvent();
 
-        public HoloKitKeyCode placeOnKey;
-        public bool placeOnTouch;
-        public bool rotateToCamera = false;
-        public GameObject holder;
-        public LayerMask placeLayerMask;
+        public PlacePointer placePointer;
+        public LayerMask placeMask;
+        public bool isPlaceOnKey = true;
+        public HoloKitKeyCode placeKey;
+        public bool isPlaceOnTouch = true;
+        public bool isRotateToCamera = false;
 
+        public GameObject holder;
+        
         private void Update()
         {
-            if (Input.GetMouseButton(0))
+            if ((isPlaceOnTouch && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) ||
+                (isPlaceOnKey && HoloKitInputManager.Instance.GetKeyDown(placeKey)))
             {
-                Camera camera = HoloKitCameraRigController.Instance.CenterCamera;
-                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-                RaycastHit rayHit;
-                if (Physics.Raycast(ray, out rayHit, float.MaxValue, placeLayerMask))
-                {
-                    holder.transform.position = rayHit.point;
-                }
+                PlaceObject();
             }
         }
 
         private void PlaceObject()
         {
+            Vector2 screenPoint = Vector2.zero;
+            Camera camera = HoloKitCameraRigController.Instance.CenterCamera;
 
+            switch (placePointer)
+            {
+                case PlacePointer.CenterScreen:
+                    screenPoint = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+                    break;
+                case PlacePointer.Touch:
+                    screenPoint = Input.mousePosition;
+                    break;
+            }
+
+            if (UITool.IsOverUI(screenPoint))
+                return;
+
+            Ray ray = camera.ScreenPointToRay(screenPoint);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, float.MaxValue, placeMask))
+            {
+                holder.SetActive(!holder.activeSelf);
+                
+                if (holder.activeSelf)
+                {
+                    holder.transform.position = rayHit.point;
+                    if (isRotateToCamera)
+                    {
+                        Quaternion lookRotation = camera.transform.rotation;
+                        holder.transform.rotation = Quaternion.Euler(new Vector3(0f, lookRotation.eulerAngles.y + 180f, 0f));
+                    }
+
+                    if (onPlace != null)
+                        onPlace.Invoke();
+                }
+                else
+                {
+                    if (onHide != null)
+                        onHide.Invoke();
+                }
+            }
         }
     }
 }
